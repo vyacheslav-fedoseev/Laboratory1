220ca5e97d06db88ab48e13dcc4c646347a5815b
vyacheslav_fedoseev <slava140901@gmail.com>
2020-10-12 17:21:22 +0300

    Files is deleted

--- src/bsu/rfe/java/group7/lab1/Fedoseev/varB6/Consumable.java
+++
@@ -1,6 +0,0 @@
-package bsu.rfe.java.group7.lab1.Fedoseev.varB6;
-
-public interface Consumable {
-    public abstract void consume();
-}
-
--- src/bsu/rfe/java/group7/lab1/Fedoseev/varB6/Nutritious.java
+++
@@ -1,5 +0,0 @@
-package bsu.rfe.java.group7.lab1.Fedoseev.varB6;
-
-public interface Nutritious {
-    public abstract double calculateCalories();
-}
